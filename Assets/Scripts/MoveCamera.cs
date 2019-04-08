e9844ae0cc7a9db3d56812e46c79ea9d519c676b
knh190 <knh190@hotmail.com>
2019-04-08 15:43:41 +0800

    update.

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -3,27 +3,34 @@
 public class MoveCamera : MonoBehaviour
 {
     public float speed = 1f;
+    public float smoothTime = 0.3F;
 
     void Move(float x, float z)
     {
         Vector3 pos = Camera.main.transform.position;
-        pos.x += x;
-        pos.z += z;
-        Camera.main.transform.position = pos;
+        Vector3 end = pos;
+        end.x += x;
+        end.z += z;
+        Camera.main.transform.position = Vector3.Lerp(pos, end, smoothTime);
+    }
+
+    void Move(Vector3 dir)
+    {
+        Move(dir.x, dir.z);
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            Move(0, -speed);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            Move(-transform.right * speed);
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            Move(0, speed);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            Move(transform.right * speed);
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            Move(speed, 0);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            Move(transform.forward * speed);
 
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            Move(-speed, 0);
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            Move(-transform.forward * speed);
     }
 }
